abe2fe79c0c3dbbd98730a3ae2ac649318c986a6
anhtay <tayphongtran@gmail.com>
2022-03-21 11:55:22 +0700

    up

--- src/ClassTay/Bai1.java
+++ src/ClassTay/Bai1.java
@@ -7,5 +7,16 @@ public class Bai1 {
         System.out.println("chay dc k");
         Date now = new Date();
         System.out.println("now = " + now);
+//  Biến i kiểu số nguyên, có giá trị là 10
+//Biến f kiểu số thực, có giá trị là 20.5
+//Biến d kiểu số thực lớn, có giá trị là 20.5
+//Biến b kiểu logic, có giá trị là true
+//Biến c kiểu ký tự, có giá trị là 'a'
+//Biến s kiểu chuỗi, có giá trị là "Hà Nội".
+        int i =10;
+        float f = 20.5f;
+        double d = 20.5d;
+        Boolean b = true;
+        String s = "Hà Nội";
     }
 }
