647a971a7c51858ccf34891c380df814c885b276
anhtay <tayphongtran@gmail.com>
2022-03-21 11:38:35 +0700

    BT1

---
+++ src/ClassTay/Bai1.java
@@ -0,0 +1,11 @@
+package ClassTay;
+
+import java.util.Date;
+
+public class Bai1 {
+    public static void main(String[] args) {
+        System.out.println("chay dc k");
+        Date now = new Date();
+        System.out.println("now = " + now);
+    }
+}
